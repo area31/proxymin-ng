e91f029b751c20a08db0a0c71cb61ab2ffdb3a6e
root <raphaelbastos@hackstore.com.br>
2023-03-11 20:42:14 -0300

    melhorias do insert hosts

--- inserthosts-proxymin.py
+++ inserthosts-proxymin.py
@@ -11,36 +11,30 @@ import objectiterator
 from globals import *
 from localization import _
 
-#Criar objeto iterator, usando a classe objectierator.py, setando a categoria GROUPS (possiveis(GROUPS, HOSTS, USERS)
+# Criar objeto iterator, usando a classe objectiterator.py, setando a categoria HOSTS
 iterator = objectiterator.ObjectIterator(CATEGORY_HOSTS)
 
-# Pegar hosts da arvore do AD
-#sys.exec("net rpc user -U proxyinternet%Proxymegapass26910 -S scbdc01")
-#userlist = os.popen("net rpc user -U proxyinternet%Proxymegapass26910 -S scbdc01 >/tmp/userlist.txt")
-
-#Ler arquivo com hosts e inserir na base
-#Abrir arquivo
-filehandleit=open('/tmp/hostlist.txt', 'r')
-#Ler cada linha
-for host in filehandleit:
-        #Remover os ultimos 2 caracteres de line (\r e \n)
+# Abrir arquivo com hosts e inserir na base
+# Verificar se o arquivo existe antes de abri-lo
+if os.path.exists('/tmp/hostlist.txt'):
+    # Abrir arquivo
+    filehandleit = open('/tmp/hostlist.txt', 'r')
+    # Ler cada linha
+    for host in filehandleit:
+        # Remover os últimos 2 caracteres de line (\r e \n)
         host = host[:-1]
-        #Se host existir, abortar
+        # Verificar se o host já existe no banco de dados
         if iterator.get_object(host):
-            print("Host " + host + " já existe na base de dados. Ignorando.")
-            continue
-        category='hosts'
-        description=''
-        group='Liberado'
-        active='checked'
-        gui_object_details.save_input_data(category, host, description, group, active)
-        #iterator.add_object(host)
-        #object = iterator.get_object(host)
-        #object.set_description('')
-        #object.set_group('Bloqueado')
-        #object.enable()
-        #iterator.update_object(object)
-        print("Host " + host + " inserido")
-
-#Commit
+            print("Host " + host + " já existe no banco de dados.")
+        else:
+            category = 'hosts'
+            description = ''
+            group = 'Liberado'
+            active = 'checked'
+            gui_object_details.save_input_data(category, host, description, group, active)
+            print("Host " + host + " inserido.")
+    # Fechar arquivo
+    filehandleit.close()
+else:
+    print("Arquivo /tmp/hostlist.txt não encontrado.")
 
