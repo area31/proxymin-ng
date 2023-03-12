6ae0419b0eddfe8a70d9553b0fa9090ed5995339
root <raphaelbastos@hackstore.com.br>
2023-03-11 21:13:21 -0300

    melhorias do insert sites on groups

--- insertsites-proxymin.py
+++ insertsites-proxymin.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
-# Script que insere uma lista de sites ou apenas um unico site no database do proxymin. 
+# Script que insere uma lista de sites ou apenas um unico site no database do proxymin.
 # Deve-se copiar esse arquivo para o diretorio raiz do proxymin para executar
 #
-#
 
+import sys
+import os
 
 import dbaccess
 import permissions
@@ -12,39 +13,55 @@ import objectiterator
 
 from globals import *
 
-#Crir objeto iterator, usando a classe objectierator.py, setando a categoria GROUPS (possiveis(GROUPS, HOSTS, USERS)
+# Verifica se um argumento foi passado para o script
+if len(sys.argv) < 2:
+    print("Uso: python3 insertsites-proxymin.py <nome_do_grupo>")
+    sys.exit(1)
+
+# Obtém o nome do grupo a partir do argumento
+group_name = sys.argv[1]
+
+# Cria objeto iterator, usando a classe objectiterator.py, setando a categoria GROUPS (possiveis(GROUPS, HOSTS, USERS)
 iterator = objectiterator.ObjectIterator(CATEGORY_GROUPS)
-#Objeto que vai utilizar o ID como o nome do grupo (Grupo-ID)
-object = iterator.get_object('Sinistro-Regulagem')
 
-#Imprimir todas URLS setadas do grupo acima
-print object.get_http_permissions().get_url_list()
+# Obtém o objeto do grupo especificado
+group_object = iterator.get_object(group_name)
 
-#Inserir URL ao Grupo acima
-#Vai inserir as urls, adicionando as ja existentes, nao grava urls repetidas
+# Verifica se o grupo existe
+if not group_object:
+    print(f"Erro: o grupo '{group_name}' não existe.")
+    sys.exit(1)
+
+# Imprime todas URLS setadas do grupo acima
+print(group_object.get_http_permissions().get_url_list())
+
+# Inserir URL ao Grupo acima
+# Vai inserir as urls, adicionando as ja existentes, nao grava urls repetidas
 
 ##############################Inserindo somente uma url:#############################
-##object.get_http_permissions().add_url('www.foi.com.br')
+##group_object.get_http_permissions().add_url('www.foi.com.br')
 ###############################################################################
 
 ########################### #Inserindo varias urls: ##############################
-#Abrir arquivo
-filehandleit=open('/root/lista-g.txt', 'r')
-#Ler cada linha
-for line in filehandleit:
-	#Remover os ultimos 2 caracteres de line (\r e \n)
-	line = line[:-1]
-	#Inserir o objeto dentro do proxymin, usando line
-	object.get_http_permissions().add_url(line)
-#Fechar handle
-filehandleit.close()
-##################################################################################
-
-
-#Imprimir novamente todas urls setadas
-print object.get_http_permissions().get_url_list()
-
-
-#Atualizar arquivo com as alteracoes efetuadas:
-iterator.update_object(object)
-
+# Abrir arquivo
+if os.path.exists('/tmp/lista-g.txt'):
+    filehandleit = open('/tmp/lista-g.txt', 'r')
+    # Ler cada linha
+    for line in filehandleit:
+        # Remover os ultimos 2 caracteres de line (\r e \n)
+        line = line[:-1]
+        # Inserir o objeto dentro do proxymin, usando line
+        group_object.get_http_permissions().add_url(line)
+    # Fechar handle
+    filehandleit.close()
+    ##################################################################################
+    
+    
+    # Imprime novamente todas urls setadas
+    print(group_object.get_http_permissions().get_url_list())
+    
+    # Atualizar arquivo com as alteracoes efetuadas:
+    iterator.update_object(group_object)
+    
+else:
+    print("Arquivo /tmp/lista-g.txt não encontrado.")
